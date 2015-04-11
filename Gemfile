4247716c139049127d7fb310a64d7e521ef33009
HafidTraikzi <hafid.traikzi@gmail.com>
2015-04-11 16:15:29 +0200

    Add pagination with Kaminari.

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'spring',        group: :development
 
 gem 'devise', '~> 3.3.0'
 
+gem 'kaminari', '~> 0.16.1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all.order("created_at DESC")
+        @articles = Article.page(params[:page]).per(10)
     end
 
     def new 
